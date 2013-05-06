c2d0a021241208cebcc11d136b9a171de37f3c2b
Matteo Crippa <matteo.crippa@gmail.com>
2013-05-06 10:34:33 +0200

    new version with sftp support and new path /var/www for websites

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@
 NGINX_CONFIG='/etc/nginx/sites-available'
 NGINX_SITES_ENABLED='/etc/nginx/sites-enabled'
 PHP_INI_DIR='/etc/php5/fpm/pool.d'
-WEB_SERVER_GROUP='www-data'
 NGINX_INIT='/etc/init.d/nginx'
 PHP_FPM_INIT='/etc/init.d/php5-fpm'
 # --------------END 
@@ -37,31 +36,16 @@ echo "Please specify the username for this site?"
 read USERNAME
 HOME_DIR=$USERNAME
 adduser $USERNAME
-# -------
-# CentOS:
-# If you're using CentOS you will need to uncomment the next 3 lines!
-# -------
-#echo "Please enter a password for the user: $USERNAME"
-#read -s PASS
-#echo $PASS | passwd --stdin $USERNAME
 
-echo "Would you like to change to web root directory (y/n)?"
-read CHANGEROOT
-if [ $CHANGEROOT == "y" ]; then
-    echo "Enter the new web root dir (after the $DOMAIN/)"
-    read DIR
-    PUBLIC_HTML_DIR='/'$DOMAIN'/'$DIR
-else
-    PUBLIC_HTML_DIR='/'$DOMAIN
-fi
+PUBLIC_HTML_DIR='/htdocs'
 
 # Now we need to copy the virtual host template
 CONFIG=$NGINX_CONFIG/$DOMAIN.conf
 cp $CURRENT_DIR/nginx.vhost.conf.template $CONFIG
 $SED -i "s/@@HOSTNAME@@/$DOMAIN/g" $CONFIG
-$SED -i "s#@@PATH@@#\/home\/"$USERNAME$PUBLIC_HTML_DIR"#g" $CONFIG
-$SED -i "s/@@LOG_PATH@@/\/home\/$USERNAME\/_logs/g" $CONFIG
-$SED -i "s#@@SOCKET@@#/var/run/"$USERNAME"_fpm.sock#g" $CONFIG
+$SED -i "s#@@PATH@@#\/var\/www\/"$DOMAIN$PUBLIC_HTML_DIR"#g" $CONFIG
+$SED -i "s/@@LOG_PATH@@/\/var/\www\/$DOMAIN\/logs/g" $CONFIG
+$SED -i "s#@@SOCKET@@#/var/run/"$DOMAIN"_fpm.sock#g" $CONFIG
 
 echo "How many FPM servers would you like by default:"
 read FPM_SERVERS
@@ -75,31 +59,38 @@ FPMCONF="$PHP_INI_DIR/$DOMAIN.pool.conf"
 cp $CURRENT_DIR/pool.conf.template $FPMCONF
 
 $SED -i "s/@@USER@@/$USERNAME/g" $FPMCONF
-$SED -i "s/@@HOME_DIR@@/\/home\/$USERNAME/g" $FPMCONF
+$SED -i "s/@@HOME_DIR@@/\/var\/www\/$DOMAIN/g" $FPMCONF
 $SED -i "s/@@START_SERVERS@@/$FPM_SERVERS/g" $FPMCONF
 $SED -i "s/@@MIN_SERVERS@@/$MIN_SERVERS/g" $FPMCONF
 $SED -i "s/@@MAX_SERVERS@@/$MAX_SERVERS/g" $FPMCONF
 MAX_CHILDS=$((MAX_SERVERS+START_SERVERS))
 $SED -i "s/@@MAX_CHILDS@@/$MAX_CHILDS/g" $FPMCONF
 
-usermod -aG $USERNAME $WEB_SERVER_GROUP
-chmod g+rx /home/$HOME_DIR
-chmod 600 $CONFIG
+# disable shell for user
+usermod -s /bin/false $USERNAME
 
+# set user groups
+adduser $USERNAME sftp
+adduser $USERNAME www-data
+
+# move the config file
+chmod 600 $CONFIG
 ln -s $CONFIG $NGINX_SITES_ENABLED/$DOMAIN.conf
 
-# set file perms and create required dirs!
-mkdir -p /home/$HOME_DIR$PUBLIC_HTML_DIR
-mkdir /home/$HOME_DIR/_logs
-mkdir /home/$HOME_DIR/_sessions
-chmod 750 /home/$HOME_DIR -R
-chmod 700 /home/$HOME_DIR/_sessions
-chmod 770 /home/$HOME_DIR/_logs
-chmod 750 /home/$HOME_DIR$PUBLIC_HTML_DIR
-chown $USERNAME:$USERNAME /home/$HOME_DIR/ -R
+# create web dirs
+mkdir -p /var/www/$DOMAIN/htdocs
+mkdir /var/www/$DOMAIN/logs
+mkdir /var/www/$DOMAIN/sessions
 
-$NGINX_INIT reload
-$PHP_FPM_INIT restart
+# set user chroot
+usermod -d /var/www/$DOMAIN $USERNAME
+chown root:root /var/www/$DOMAIN
+
+# set directory permission
+chown -R $USERNAME:$USERNAME /var/www/$DOMAIN/htdocs 
+chmod -R g+rw /var/www/$DOMAIN/htdocs
+
+#$NGINX_INIT reload
+#$PHP_FPM_INIT restart
 
 echo -e "\nSite Created for $DOMAIN with PHP support"
-
