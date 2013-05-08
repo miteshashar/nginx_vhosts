8942934b5a4690037017ee74e09855ccc59676df
Matteo Crippa <matteo.crippa@gmail.com>
2013-05-08 10:25:15 +0200

    new structure and /srv/www

--- setup.sh
+++ setup.sh
@@ -5,6 +5,17 @@
 # Modified:   07/01/2012
 # Modified:   27/11/2012
 # Modified: 05/06/2013 - Added support for sftp + /var/www < Matteo Crippa >
+# Modified: 08/06/2013 - Moved to /srv/www and subdomain support
+
+# TODO: new way to handle, system asks for domain, then for subdomain (www and no-www) are used at the same way.
+# TODO: testing
+
+# FS structure:
+# /srv/www/domain/subdomain/htdocs
+# /srv/www/domain/subdomain/_session
+# /srv/www/domain/subdomain/_logs
+
+
 
 # Modify the following to match your system
 NGINX_CONFIG='/etc/nginx/sites-available'
@@ -32,16 +43,17 @@ else
     exit 1 
 fi
 
+# Keep default dir for deploy
+PUBLIC_HTML_DIR='/htdocs'
 
 # Ask for subdomain (default www)
 echo "Are you configuring the www or no-www domain (y/n)?"
 read CHANGEROOT
 if [ $CHANGEROOT == "n" ]; then
-    echo "Enter the subdomain you are configuring"
-    read DIR
-    PUBLIC_HTML_DIR='/'$DIR
+    echo "Enter the subdomain you are configuring: (without domain and extension)"
+    read SUB
 else
-    PUBLIC_HTML_DIR='/www'
+    SUB='www'
 fi
 
 # Create a new user
@@ -50,12 +62,12 @@ read USERNAME
 useradd $USERNAME
 
 # Now we need to copy the virtual host template
-CONFIG=$NGINX_CONFIG/$DOMAIN.conf
+CONFIG=$NGINX_CONFIG/$SUB.$DOMAIN.conf
 cp $CURRENT_DIR/nginx.vhost.conf.template $CONFIG
 $SED -i "s/@@HOSTNAME@@/$DOMAIN/g" $CONFIG
-$SED -i "s#@@PATH@@#\/var\/www\/"$DOMAIN$PUBLIC_HTML_DIR"#g" $CONFIG
-$SED -i "s/@@LOG_PATH@@/\/var\/www\/$DOMAIN\/logs/g" $CONFIG
-$SED -i "s#@@SOCKET@@#/var/run/"$DOMAIN"_fpm.sock#g" $CONFIG
+$SED -i "s#@@PATH@@#\/srv\/www\/"$DOMAIN\/$SUB\/$PUBLIC_HTML_DIR"#g" $CONFIG
+$SED -i "s/@@LOG_PATH@@/\/srv\/www\/$DOMAIN\/$SUB\/_logs/g" $CONFIG
+$SED -i "s#@@SOCKET@@#/var/run/"$SUB"."$DOMAIN"_fpm.sock#g" $CONFIG
 
 echo "How many FPM servers would you like by default: (suggested 2)"
 read FPM_SERVERS
@@ -64,13 +76,13 @@ read MIN_SERVERS
 echo "Max number of FPM servers would you like: (suggested 5)"
 read MAX_SERVERS
 # Now we need to create a new php fpm pool config
-FPMCONF="$PHP_INI_DIR/$DOMAIN.pool.conf"
+FPMCONF="$PHP_INI_DIR/$SUB.$DOMAIN.pool.conf"
 
 cp $CURRENT_DIR/pool.conf.template $FPMCONF
 
 $SED -i "s/@@USER@@/$USERNAME/g" $FPMCONF
 $SED -i "s/@@DOMAIN@@/$DOMAIN/g" $FPMCONF
-$SED -i "s/@@HOME_DIR@@/\/var\/www\/$DOMAIN/g" $FPMCONF
+$SED -i "s/@@HOME_DIR@@/\/srv\/www\/$DOMAIN\/$SUB/g" $FPMCONF
 $SED -i "s/@@START_SERVERS@@/$FPM_SERVERS/g" $FPMCONF
 $SED -i "s/@@MIN_SERVERS@@/$MIN_SERVERS/g" $FPMCONF
 $SED -i "s/@@MAX_SERVERS@@/$MAX_SERVERS/g" $FPMCONF
@@ -86,23 +98,24 @@ adduser $USERNAME www-data
 
 # move the config file
 chmod 600 $CONFIG
-ln -s $CONFIG $NGINX_SITES_ENABLED/$DOMAIN.conf
+ln -s $CONFIG $NGINX_SITES_ENABLED/$SUB.$DOMAIN.conf
 
 # create web dirs
-mkdir -p /var/www/$DOMAIN/www
-mkdir /var/www/$DOMAIN/logs
-mkdir /var/www/$DOMAIN/sessions
+mkdir -p /srv/www/$DOMAIN/$SUB/$PUBLIC_HTML_DIR
+mkdir /srv/www/$DOMAIN/$SUB/_logs
+mkdir /srv/www/$DOMAIN/$SUB/_sessions
 
 # set user chroot
-usermod -d /var/www/$DOMAIN $USERNAME
-chown root:root /var/www/$DOMAIN
+usermod -d /srv/www/$DOMAIN/$SUB $USERNAME
+chown root:root /srv/www/$DOMAIN/$SUB
 
 # set directory permission
-chown -R $USERNAME:$USERNAME /var/www/$DOMAIN/www 
-chmod -R g+rw /var/www/$DOMAIN/www
+chown -R $USERNAME:$USERNAME /srv/www/$DOMAIN/$SUB/$PUBLIC_HTML_DIR 
+chmod -R g+rw /srv/www/$DOMAIN/$SUB/$PUBLIC_HTML_DIR
 
+# restart services
 $NGINX_INIT reload
 $PHP_FPM_INIT restart
 
-echo -e "\nSite Created for $DOMAIN with PHP support"
+echo -e "\nSite Created for $SUB.$DOMAIN with PHP support"
 echo -e "\n\nIMPORTANT you need to set $USERNAME password using passwd"
